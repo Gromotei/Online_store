63e78bbf22f1b7391c456da6854c6babeaaa3535
Сергей Моисеенко <maes10065@gmail.com>
2023-02-10 20:44:44 +0300

    backend first commit

---
+++ server/controllers/userController.js
@@ -0,0 +1,21 @@
+const ApiErrors = require('../error/ApiError')
+const ApiError = require("../error/ApiError");
+class UserController {
+    async registration(req, res) {
+
+    }
+
+    async login(req, res) {
+
+    }
+
+    async check(req, res, next) {
+        const {id} = req.query
+        if (!id) {
+           return  next(ApiError.badRequest('Не задан ID'))
+        }
+        res.json(id);
+    }
+}
+
+module.exports = new UserController()
